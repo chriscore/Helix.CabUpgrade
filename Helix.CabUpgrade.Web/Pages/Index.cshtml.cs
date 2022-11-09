db74ecea0be33b4915f7e32244a1fc1a063b1b6e
chriscore <chriscore361@googlemail.com>
2022-11-09 23:12:46 +0000

    Display errors on index page

--- Helix.CabUpgrade.Web/Pages/Index.cshtml.cs
+++ Helix.CabUpgrade.Web/Pages/Index.cshtml.cs
@@ -38,11 +38,24 @@ namespace Helix.CabUpgrade.Web.Pages
         [BindProperty]
         public bool ForceOverrideSecondaryCab { get; set; }
 
+        [BindProperty]
+        public bool IsError { get; set; }
+        [BindProperty]
+        public string ErrorMessage { get; set; }
+
         public async Task<ActionResult> OnPostAsync()
         {
             try
             {
+                if (Upload == null || Upload.Length == 0)
+                {
+                    throw new InvalidOperationException("Please upload a valid helix .hlx preset file, using the 'Choose file' button.");
+                }
                 var json = await ReadFormFileAsync(Upload);
+                if (string.IsNullOrEmpty(json))
+                {
+                    throw new InvalidOperationException("Please upload a valid helix .hlx preset file, using the 'Choose file' button.");
+                }
                 _logger.LogInformation($"Received preset upload: {json}");
 
                 var defaults = new PresetUpdaterDefaults()
@@ -55,13 +68,20 @@ namespace Helix.CabUpgrade.Web.Pages
                 var result = _presetUpdater.UpdatePresetJson(json, defaults);
 
                 byte[] bytes = Encoding.ASCII.GetBytes(result.PresetJson);
+                ErrorMessage = "";
+                IsError = false;
                 return File(bytes, Upload.ContentType, $"{result.PatchName}.hlx");
             }
             catch (Exception e)
             {
+                /*
                 _logger.LogError(e, "Error occurred during user preset upgrade. Please get in touch via email to report a bug to chriscore361@googlemail.com");
                 Response.StatusCode = 400;
-                return Content(e.Message);
+                return Content($"{e.Message}{Environment.NewLine}{Environment.NewLine}{e.StackTrace}");
+                */
+                ErrorMessage = $"Something went wrong during user preset upgrade: {e.Message}";
+                IsError = true;
+                return Page();
             }
         }
 
