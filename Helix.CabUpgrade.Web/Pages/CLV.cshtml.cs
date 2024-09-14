e640f73b76997bafdb75d0182ea04ff007c2bbb1
chriscore <chriscore361@googlemail.com>
2024-09-14 16:03:34 +0100

    Adding CLV

---
+++ Helix.CabUpgrade.Web/Pages/CLV.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Helix.CabUpgrade.Web.Pages
+{
+    public class CLVModel : PageModel
+    {
+        private readonly ILogger<CLVModel> _logger;
+
+        public CLVModel(ILogger<CLVModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
